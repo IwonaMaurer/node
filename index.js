dd4d76f30b6982a016e4c1e78429e4e9f3cf1a1c
IwonaMaurer <plastimex2@wp.pl>
2017-12-01 15:24:44 +0100

    zadanie

--- index.js
+++ index.js
@@ -51,22 +51,3 @@ OSinfo.print();
 
 
 
-/*
-function formatUptime(uptime) {
-    var unit = 'second';
-    if (uptime > 60) {
-        uptime = uptime / 60;
-        unit = 'minute';
-    }
-    if (uptime > 60) {
-        uptime = uptime / 60;
-        unit = 'hour';
-    }
-    if (uptime != 1) {
-        unit = unit + 's';
-    }
-
-    uptime = uptime + ' ' + unit;
-    return uptime;
-}
-*/
